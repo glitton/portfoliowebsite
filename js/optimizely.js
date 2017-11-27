76b33c32aa988f1d898fe6eb8bf0a3514cb561e1
Generosa Litton <generosa.litton@yahoo.com>
2017-11-26 22:16:27 -0800

    added spotify image

---
+++ js/optimizely.js
@@ -0,0 +1,13 @@
+//Optimizely Script for testing CTA button
+window['optimizely'] = window['optimizely'] || [];
+window['optimizely'].push({
+  type: "event",
+  eventName: "CTA Welcome Page"
+});
+
+//Optimizely script for testing download resume
+window['optimizely'] = window['optimizely'] || [];
+window['optimizely'].push({
+  type: "event",
+  eventName: "Resume"
+});
